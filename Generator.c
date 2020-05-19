7e4c959d6f592e95816c39b5751f443e44cb8d96
Psyrkoz <psyrkoz68@gmail.com>
2020-05-19 21:39:34 +0200

    Letrucfinallol

--- Generator.c
+++ Generator.c
@@ -1,12 +1,15 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/stat.h>
+#include <sys/signal.h>
 #include <sys/shm.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h> 
 #include "defs.h"
+
+int shmID;
 
 struct Road 
 {
@@ -17,42 +20,112 @@ struct Road
 struct Voiture
 {
     int origine;
-    int routeActuelle;
+    struct Road *routeActuelle;
     int destination;
+    int numero;
 };
 
 struct Voiture creeVoiture()
 {
     struct Voiture v;
-    v.origine = rand() % 4;
-    v.routeActuelle = -1;
-    v.destination = rand() %4;
+    v.origine = rand() % NB_VAL;
+    v.routeActuelle = NULL;
+    v.destination = rand() % NB_VAL;
+    v.numero = rand();
 
     return v;
 }
 
-struct Road* creeRoute()
+int carrefourBloque(int mutex)
 {
-    struct Road* r = malloc(4*sizeof(struct Road));
-    int i;
-    for(i = 0; i < 3; i++)
+    int i = 0;
+    
+    while(i < NB_VAL && semctl(mutex, i, GETVAL) == 0)
+        i++;
+    
+    return i == NB_VAL;
+}
+
+int Drive(struct Voiture *v, struct Road* roads, int mutex)
+{
+    int i = 0;
+    fprintf(stderr, "Valeur du semaphore: %d %d %d %d\n", semctl(mutex, 0, GETVAL), semctl(mutex, 1, GETVAL), semctl(mutex, 2, GETVAL), semctl(mutex, 3, GETVAL));
+
+    if((v->routeActuelle != NULL && carrefourBloque(mutex) == 1))
     {
-        r[i].num = i;
-        r[i].next = &r[i+1];
+        fprintf(stderr, "Moi etre bloque dans carrefour mais moi partir de la route %d\n", v->routeActuelle->num);
+        
+        struct sembuf buffer;
+        buffer.sem_num = v->routeActuelle->num;
+        buffer.sem_op = 1;
+
+        semop(mutex, &buffer, 1);
+        return 0;
     }
+    else if(v->routeActuelle != NULL && (v->routeActuelle->num == v->destination))
+    {
+        fprintf(stderr, "Moi etre arriver a ma destination (%d)! PogChamp\n", v->destination);
+        
+        struct sembuf buffer;
+        buffer.sem_num = v->routeActuelle->num;
+        buffer.sem_op = 1;
 
-    r[3].num = 3;
-    r[3].next = &r[0];
+        semop(mutex, &buffer, 1);
+        fprintf(stderr, "Et grace a moid, y'a peut etre un 1: %d %d %d %d\n", semctl(mutex, 0, GETVAL), semctl(mutex, 1, GETVAL), semctl(mutex, 2, GETVAL), semctl(mutex, 3, GETVAL));
+        return 0;
+    }
+    else if (v->routeActuelle == NULL) // Salut, j'suis pas encore sur le crossroad du coups j'me rend sur l'origine
+    {
+        struct sembuf buffer;
+        buffer.sem_num = v->origine;
+        buffer.sem_op = -1;
+
+        fprintf(stderr, "Moi vouloir entrer dans route %d\n", v->origine);
+        semop(mutex, &buffer, 1);
+        fprintf(stderr, "Moi etre entrer dans route %d\n", v->origine);
+        v->routeActuelle = &roads[v->origine];
+        return 1;
+    }
+    else
+    {
+        struct sembuf buffer;
+        buffer.sem_num = v->routeActuelle->next->num;
+        buffer.sem_op = -1;
+
+        fprintf(stderr, "Moi vouloir entrer dans route %d\n", v->routeActuelle->next->num);
+        semop(mutex, &buffer, 1);
+        fprintf(stderr, "Moi etre entrer dans route %d\n", v->routeActuelle->next->num);
+
+        fprintf(stderr, "Moi vouloir partir de route %d\n", v->routeActuelle->num);
 
 
-    return r;
+        struct sembuf bufferDeux;
+        bufferDeux.sem_num = v->routeActuelle->num;
+        bufferDeux.sem_op = 1;
+
+        semop(mutex, &bufferDeux, 1);
+        fprintf(stderr, "Moi etre partir de route %d\n", v->routeActuelle->num);
+        v->routeActuelle = v->routeActuelle->next;
+        return 1;
+    }
+    return 1;
+}
+
+void handle_ctrl_c()
+{
+    printf("Suppression de la memoire partage...\n");
+    shmctl(shmID, IPC_RMID, NULL);
+    exit(0);
 }
 
 int main()
 {
-    struct sembuf* routesbuf;
-    int shmID;
+    struct Road* routes;
+    int mutex;
 
+    signal(SIGINT, handle_ctrl_c);
+
+    // Récupère le shm id
     shmID = shmget((key_t)SHM, 0, 0);
     if(shmID == -1)
     {
@@ -60,20 +133,21 @@ int main()
         exit(1);
     }
 
-    routesbuf = (struct sembuf*)shmat(shmID, NULL, 0);
-    if(routesbuf == (struct sembuf*)-1)
+    // Récupère le semid
+    mutex = semget((key_t)SEMA, 0, 0);
+    if(mutex == -1)
+    {
+        fprintf(stderr, "Impossible de recuperer le mutex\n");
+    }
+
+    // Récupère le buffer depuis la mémoire partagé
+    routes = (struct Road*)shmat(shmID, NULL, 0);
+    if(routes == (struct Road*)-1)
     {
         perror("Impossible d'attacher le tableau en memoire partage...\n");
         exit(2);
-    }
+    } 
 
-    int i = 0;
-    for(i; i < NB_VAL; i++)
-    {
-        printf("Nombre: %d, Operation: %d,  Flag: %d\n", routesbuf[i].sem_num, routesbuf[i].sem_op, routesbuf[i].sem_flg);
-    }
-
-    struct Road* r = creeRoute();    
     while(1)
     {
         pid_t c = fork();
@@ -81,12 +155,16 @@ int main()
         {
             srand(time(0));
             struct Voiture v = creeVoiture();
-            fprintf(stderr, "%d %d %d\n", v.origine, v.destination, v.routeActuelle);
-            break;
+            while(Drive(&v, routes, mutex) == 1)
+            {
+                sleep(3);
+            }
+            exit(0);
         }
         else
         {
-            sleep(1);
+            sleep((rand()% 5) + 5);
         }
     }
+    return 0;
 }
--- main.c
+++ main.c
@@ -17,51 +17,41 @@ int creeSemaphore()
     return semget((key_t)SEMA, NB_VAL, IPC_CREAT | S_IRUSR | S_IWUSR);
 }
 
-struct sembuf* initialiserSemaphore(int mutex, int init_value, struct sembuf* routes)
+void initialiserSemaphore(int mutex, int init_value)
 {
-    //struct sembuf* routes = malloc(NB_VAL*sizeof(struct sembuf));
     int i;
     for(i = 0; i < NB_VAL; i++)
     {
         semctl(mutex, i, SETVAL, init_value);
-        routes[i].sem_num = i;
-        routes[i].sem_op  = semctl(mutex, i, GETVAL);
-        routes[i].sem_flg = 0;
     }
-    
-    return routes;
 }
 
-void displaySemaphore(int mutex)
+void initialiserRoad(struct Road* routes)
 {
-    printf("--> Acquisition de %d mutex pour PID=%d\n", NB_VAL, getpid());
-
-    int i = 0;
-    for(i; i < NB_VAL; i++)
+    int i;
+    for(i = 0; i < NB_VAL - 1; i++)
     {
-        printf("--> Valeur %deme semaphore=%d pour PID=%d\n", i+1, semctl(mutex, i, GETVAL), getpid());
+        routes[i].num  = i;
+        routes[i].next = &routes[i+1];
     }
-}
 
-int carrefourBloque(int mutex)
-{
-    int i = 0;
-    while(i < NB_VAL && semctl(mutex, i, GETVAL) == 0)
-        i++;
-
-    return i != NB_VAL;
+    routes[NB_VAL - 1].num = NB_VAL - 1;
+    routes[NB_VAL - 1].next = &routes[0];
 }
 
 int main()
 {
-    int shmID = shmget((key_t)SHM, NB_VAL*sizeof(struct sembuf), IPC_CREAT |  S_IRUSR | S_IWUSR);
-    struct sembuf* routes = (struct sembuf*)shmat(shmID, 0, NULL);
+    int shmID = shmget((key_t)SHM, NB_VAL*sizeof(struct Road), IPC_CREAT |  S_IRUSR | S_IWUSR);
+    struct Road* routes = (struct Road*)shmat(shmID, 0, NULL);
 
     int mutex = creeSemaphore();
-    initialiserSemaphore(mutex, 0, routes);
+    initialiserSemaphore(mutex, 1);
+    initialiserRoad(routes);
 
     int i;
+    printf("Valeur du semaphore:\n");
     for(i = 0; i < NB_VAL; i++)
-        printf("Nombre: %d, Operation: %d,  Flag: %d\n", routes[i].sem_num, routes[i].sem_op, routes[i].sem_flg);
+        printf("\tRang: %d; Valeur: %d;\n", i, semctl(mutex, i, GETVAL));
+        
     return 0;
 }
